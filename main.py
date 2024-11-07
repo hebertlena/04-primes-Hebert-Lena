8a723cffaf5be9a9b6029cdee1935349304bb278
hebertl <lena.hebert@edu.esiee.fr>
2024-11-07 07:33:36 +0000

    creation de la fonction secondaire, supression de import sqrt, ajout de trois test dans main et ajout de la doc string

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from math import sqrt
+""" programme est premier"""
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
+    """fonction pour determiner si un nombre est premier"""
     # votre code ici
-
-    pass
+    premier = True
+    if p==1:
+        premier =False
+    for diviseur in range(2,p) :
+        if p % diviseur == 0 :
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """fonction principale"""
     # vos appels à la fonction secondaire ici
-
+    print(isprime(3))
+    print(isprime(5))
+    print(isprime(6))
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
